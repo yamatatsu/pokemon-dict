e0f0c3bb164dec56913936f5951497642dd01f67
yamatatsu <the.kinnikuman@gmail.com>
2025-04-12 16:15:27 +0900

    feat: detail of pokemon

---
+++ src/hooks/useSpecie.ts
@@ -0,0 +1,95 @@
+import { useQuery } from "urql";
+import { graphql } from "../gql";
+
+const query = graphql(`
+query specieQuery($id: Int) {
+  pokemon_v2_pokemonspeciesname(
+    where: {language_id: {_eq: 1}, pokemon_species_id: {_eq: $id}}
+    limit: 10
+  ) {
+    name
+    genus
+    pokemon_species_id
+    pokemon_v2_pokemonspecy {
+      pokemon_v2_pokemons(where: {is_default: {_eq: true}}) {
+        height
+        weight
+        pokemon_v2_pokemonmoves(
+          where: {
+            version_group_id: {_eq: 25},
+            pokemon_v2_movelearnmethod: {name: {_eq: "level-up"}}
+          }
+        ) {
+          level
+          pokemon_v2_move {
+            accuracy
+            move_effect_chance
+            pokemon_v2_movenames(where: {language_id: {_eq: 1}}) {
+              name
+            }
+            pokemon_v2_moveflavortexts(where: {language_id: {_eq: 1}, version_group_id: {_eq: 20}}) {
+              flavor_text
+            }
+            pokemon_v2_movedamageclass {
+              pokemon_v2_movedamageclassnames(where: {language_id: {_eq: 1}}) {
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`);
+
+type SpecieDetail = {
+	pokemon_species_id?: number | null;
+	name: string;
+	genus: string;
+	height?: number | null;
+	weight?: number | null;
+	moves?: {
+		level: number;
+		name?: string;
+		accuracy?: number | null;
+		move_effect_chance?: number | null;
+		flavor_text?: string;
+		damage_class?: string;
+	}[];
+};
+
+export function useSpecie(id: string): {
+	data: SpecieDetail | undefined;
+} {
+	const [result] = useQuery({
+		query: query,
+		variables: { id: Number(id) },
+	});
+
+	const data = result.data?.pokemon_v2_pokemonspeciesname?.[0];
+
+	return {
+		data: data && {
+			pokemon_species_id: data.pokemon_species_id,
+			name: data.name,
+			genus: data.genus,
+			height: data.pokemon_v2_pokemonspecy?.pokemon_v2_pokemons[0]?.height,
+			weight: data.pokemon_v2_pokemonspecy?.pokemon_v2_pokemons[0]?.weight,
+			moves:
+				data.pokemon_v2_pokemonspecy?.pokemon_v2_pokemons[0]?.pokemon_v2_pokemonmoves.map(
+					(move) => ({
+						level: move.level,
+						name: move.pokemon_v2_move?.pokemon_v2_movenames[0]?.name,
+						accuracy: move.pokemon_v2_move?.accuracy,
+						move_effect_chance: move.pokemon_v2_move?.move_effect_chance,
+						flavor_text:
+							move.pokemon_v2_move?.pokemon_v2_moveflavortexts[0]?.flavor_text,
+						damage_class:
+							move.pokemon_v2_move?.pokemon_v2_movedamageclass
+								?.pokemon_v2_movedamageclassnames[0]?.name,
+					}),
+				),
+		},
+	};
+}
--- src/routes/species.tsx
+++ src/routes/species.tsx
@@ -3,9 +3,11 @@ import {
 	Box,
 	Button,
 	Header,
+	KeyValuePairs,
 	SpaceBetween,
 	SplitPanel,
 	Table,
+	Tabs,
 	TextFilter,
 } from "@cloudscape-design/components";
 import {
@@ -15,6 +17,7 @@ import {
 } from "@tanstack/react-router";
 import { useState } from "react";
 import { PDSideNavigation } from "../components/PDSideNavigation";
+import { useSpecie } from "../hooks/useSpecie";
 import { useSpecies } from "../hooks/useSpecies";
 
 export const Route = createFileRoute("/species")({
@@ -97,9 +100,94 @@ function SpecieTable() {
 }
 
 function SpecieDetailSplitPanel() {
+	const params = useParams({ strict: false });
+	const specie = useSpecie(params.specieId ?? "");
+
+	if (!specie.data) {
+		return null;
+	}
+
 	return (
-		<SplitPanel hidePreferencesButton closeBehavior="hide" header="ポケモン">
-			ポケモン
+		<SplitPanel
+			hidePreferencesButton
+			closeBehavior="hide"
+			header={specie.data.name}
+		>
+			<Tabs
+				tabs={[
+					{
+						id: "general",
+						label: "概要",
+						content: (
+							<KeyValuePairs
+								columns={3}
+								items={[
+									{
+										label: "#",
+										value: specie.data.pokemon_species_id,
+									},
+									{
+										label: "名前",
+										value: specie.data.name,
+									},
+									{
+										label: "分類",
+										value: specie.data.genus,
+									},
+									{
+										label: "高さ",
+										value: specie.data.height
+											? `${specie.data.height / 10} m`
+											: "不明",
+									},
+									{
+										label: "体重",
+										value: specie.data.weight
+											? `${specie.data.weight / 10} kg`
+											: "不明",
+									},
+								]}
+							/>
+						),
+					},
+					{
+						id: "moves",
+						label: "わざ",
+						content: (
+							<Table
+								columnDefinitions={[
+									{ id: "level", header: "レベル", cell: (item) => item.level },
+									{ id: "name", header: "名前", cell: (item) => item.name },
+									{
+										id: "genus",
+										header: "分類",
+										cell: (item) => item.damage_class,
+									},
+									{
+										id: "description",
+										header: "説明",
+										cell: (item) => item.flavor_text,
+									},
+								]}
+								items={specie.data.moves ?? []}
+								loading={specie.data.moves?.length === 0}
+								loadingText="Loading moves"
+								empty={
+									<Box
+										margin={{ vertical: "xs" }}
+										textAlign="center"
+										color="inherit"
+									>
+										<SpaceBetween size="m">
+											<b>No moves</b>
+										</SpaceBetween>
+									</Box>
+								}
+							/>
+						),
+					},
+				]}
+			/>
 		</SplitPanel>
 	);
 }
